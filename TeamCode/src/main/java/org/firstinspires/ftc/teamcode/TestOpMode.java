6b04256c77187c6e4b65e363bb920d331d4bf7fb
CLiF <clif1593@gmail.com>
2024-01-12 21:28:30 +0900

    Set Environment

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@TeleOp(name = "TeleOp", group = "")
+public class TestOpMode extends OpMode {
+    @Override
+    public void init()
+    {
+
+    }
+
+    @Override
+    public void loop()
+    {
+
+    }
+}
