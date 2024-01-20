2037b72312ac6f83d2a530bea69fb56e675ad88a
Joonsung Kim <js.tt0421k@gmail.com>
2024-01-20 13:33:34 +0900

    apriltag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testop/low/AprilTag_TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testop/low/AprilTag_TestOpMode.java
@@ -16,9 +16,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 
 import org.firstinspires.ftc.teamcode.hardware.IMUHW;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -26,10 +28,10 @@ import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 @TeleOp(name = "AprilTag_TestOpMode", group = "Low")
 public class AprilTag_TestOpMode extends OpMode {
-
+    public AprilTagProcessor myAprilTagProcessor;
     @Override
     public void init() {
-        AprilTagProcessor myAprilTagProcessor;
+
         AprilTagLibrary myAprilTagLibrary = AprilTagGameDatabase.getCurrentGameTagLibrary();
 
         myAprilTagProcessor = new AprilTagProcessor.Builder()
@@ -68,9 +70,47 @@ public class AprilTag_TestOpMode extends OpMode {
 
     @Override
     public void loop() {
+        List<AprilTagDetection> myAprilTagDetections;  // list of all detections
+        AprilTagDetection myAprilTagDetection;         // current detection in for() loop
+        int myAprilTagIdCode;                           // ID code of current detection, in for() loop
+        String myAprilTagName;
+        double myAprilTagSize;
 
-        
+        myAprilTagDetections = myAprilTagProcessor.getDetections();
 
+        for (int i = 0; i < myAprilTagDetections.size(); i++) {
+            myAprilTagDetection = myAprilTagDetections.get(i);
+            if (myAprilTagDetection.metadata != null) {  // This check for non-null Metadata is not needed for reading only ID code.
+                myAprilTagIdCode = myAprilTagDetection.id;
+                myAprilTagName = myAprilTagDetection.metadata.name;
+                myAprilTagSize = myAprilTagDetection.metadata.tagsize;
+
+                double myTagPoseX = myAprilTagDetection.ftcPose.x;
+                double myTagPoseY = myAprilTagDetection.ftcPose.y;
+                double myTagPoseZ = myAprilTagDetection.ftcPose.z;
+                double myTagPosePitch = myAprilTagDetection.ftcPose.pitch;
+                double myTagPoseRoll = myAprilTagDetection.ftcPose.roll;
+                double myTagPoseYaw = myAprilTagDetection.ftcPose.yaw;
+
+                double myTagPoseRange = myAprilTagDetection.ftcPose.range;
+                double myTagPoseBearing = myAprilTagDetection.ftcPose.bearing;
+                double myTagPoseElevation = myAprilTagDetection.ftcPose.elevation;
+
+                telemetry.addData("IdCode", myAprilTagIdCode);
+                telemetry.addData("Size\n", myAprilTagSize);
+                telemetry.addData("PoseX", myTagPoseX);
+                telemetry.addData("PoseY", myTagPoseY);
+                telemetry.addData("PoseZ", myTagPoseZ);
+                telemetry.addData("Pitch", myTagPosePitch);
+                telemetry.addData("Yaw", myTagPoseYaw);
+                telemetry.addData("Roll", myTagPoseRoll);
+                telemetry.addData("Range", myTagPoseRange);
+                telemetry.addData("Bearing", myTagPoseBearing);
+                telemetry.addData("Elevation", myTagPoseElevation);
+            }
+        }
+
+        telemetry.update();
     }
 
 }
